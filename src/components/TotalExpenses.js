d4fb2166f7f3d019333d907abbae3fad4e7e6803
PranavPrashar <prasherlucky@gmail.com>
2024-10-05 15:29:04 -0400

    Combining top section into one component, has the chart and all in one place. Also mobile freindly now

--- src/components/TotalExpenses.js
+++ src/components/TotalExpenses.js
@@ -1,18 +1,45 @@
 import ExpensePieChart from "./ExpensePieChart";
 
 function TotalExpenses({ total, expenseData }) {
+  const totalExpensesByCategory = expenseData.reduce((totals, expense) => {
+    const { category } = expense;
+    if (!totals[category]) {
+      totals[category] = 0;
+    }
+    totals[category] += parseFloat(expense.amount) || 0;
+    return totals;
+  }, {});
+
+  console.log(totalExpensesByCategory);
+
   return (
     <div className="mb-6">
       <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
 
-      <div className="bg-white p-6 rounded-lg shadow-md flex justify-between items-center">
-        
-        <div className="w-1/3">
-          <h2 className="text-xl font-bold">Total Expenses</h2>
-          <p className="text-3xl mt-4">${total}</p>
+      <div className="bg-white p-6 rounded-lg shadow-md flex md:flex-row flex-col justify-between">
+        <div className="md:w-1/3 flex md:flex-col md:items-start md:justify-start flex-row items-center justify-between w-full">
+          <h2 className="text-xl font-bold">Total Expenses: </h2>
+          <p className="text-3xl md:mt-4">${total}</p>
         </div>
 
-        <div className="w-2/3" style={{ width: '250px', height: '250px', margin: '0 auto' }}>
+        <div className="md:w-1/3 md:flex-col flex-row py-4">
+          <h3 className="text-lg font-bold">Expenses Categorized by Type:</h3>
+          <ul>
+            {Object.entries(totalExpensesByCategory).map(
+              ([category, total]) => (
+                <li key={category} className="mt-2">
+                  <span className="font-semibold">{category}:</span> $
+                  {total.toFixed(2)}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+
+        <div
+          className="w-1/3"
+          style={{ width: "250px", height: "250px", margin: "0 auto" }}
+        >
           <ExpensePieChart expenses={expenseData} />
         </div>
       </div>
